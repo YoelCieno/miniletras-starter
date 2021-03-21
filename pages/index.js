a9d71f6916c1651dd53fb27dddcfcb16b70220c0
YoelCieno <frmonteira@gmail.com>
2021-03-21 01:21:12 +0100

    feat(app): first commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Button from '@components/Button'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>MiniLetras</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Cocina tu cuento favorito" />
+        <section className="grid-container">
+          <article className="fraction"><Button /></article>
+          <article className="fraction">2</article>
+          <article className="fraction">3</article>
+          <article className="fraction">4</article>
+          <article className="fraction">5</article>
+          <article className="fraction">6</article>
+        </section>
       </main>
 
       <Footer />
